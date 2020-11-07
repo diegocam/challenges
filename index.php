31aa6d3f9fc889c88e657c0bd264e70b4b51d041
Diego Camacho <camachojcd@gmail.com>
2020-11-06 21:58:17 -0600

    Add shiftLetters function

--- index.php
+++ index.php
@@ -25,3 +25,31 @@ $egg = $chicken->layEgg();
 dump($egg);
 $bird = $egg->hatch();
 dump($bird);
+
+
+/******** QUESTION 3 ********/
+function shiftLetters(string $str, array $shifts)
+{
+    $strArr = str_split($str);
+    foreach ($shifts as $shift) {
+        $direction = $shift[0];
+        $shiftTimes = $shift[1];
+        while ($shiftTimes > 0) {
+            if ($direction === 0) {
+                // move left
+                $letter = array_shift($strArr);
+                $strArr[] = $letter;
+            } else {
+                // move  right
+                $letter = array_pop($strArr);
+                array_unshift($strArr, $letter);
+            }
+            $shiftTimes--;
+        }
+    }
+    return implode('', $strArr);
+}
+$str = "abcdefg";
+$shifts = [[1,1],[1,1],[0,2],[1,3]];
+$result = shiftLetters($str, $shifts);
+dump($result);
