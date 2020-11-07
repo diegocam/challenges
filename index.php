ea9dc64700b887648e7baee3e09280c141cf8b45
Diego Camacho <camachojcd@gmail.com>
2020-11-06 22:13:53 -0600

    Add better structure to running commands

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ use App\Birds\Chicken;
 use App\Filesystem\FileSystem;
 
 /******** QUESTION 1 ********/
+dump('QUESTION 1:');
 $f = new FileSystem();
 $f->addDirectory('directoryA', null);
 $f->addFile('fileA.jpg', 'directoryA');
@@ -17,17 +18,19 @@ $f->addFile('fileD.jpg', 'directoryC');
 dump(implode(', ', $f->getItems('directoryA')));
 $f->deleteSubdirectory('directoryB');
 $f->deleteFile('directoryA', 'fileA.jpg');
-
+dump('-----------------------------------');
 
 /******** QUESTION 2 ********/
+dump('QUESTION 2:');
 $chicken = new Chicken();
 $egg = $chicken->layEgg();
 dump($egg);
 $bird = $egg->hatch();
 dump($bird);
-
+dump('-----------------------------------');
 
 /******** QUESTION 3 ********/
+dump('QUESTION 3:');
 function shiftLetters(string $str, array $shifts)
 {
     $strArr = str_split($str);
@@ -40,7 +43,7 @@ function shiftLetters(string $str, array $shifts)
                 $letter = array_shift($strArr);
                 $strArr[] = $letter;
             } else {
-                // move  right
+                // move right
                 $letter = array_pop($strArr);
                 array_unshift($strArr, $letter);
             }
@@ -50,6 +53,13 @@ function shiftLetters(string $str, array $shifts)
     return implode('', $strArr);
 }
 $str = "abcdefg";
-$shifts = [[1,1],[1,1],[0,2],[1,3]];
+$shifts = [[1, 1], [1, 1], [0, 2], [1, 3]];
 $result = shiftLetters($str, $shifts);
 dump($result);
+dump('-----------------------------------');
+
+
+/******** QUESTION 4 ********/
+dump('QUESTION 4:');
+$sql = file_get_contents("db_query.sql");
+dump($sql);
